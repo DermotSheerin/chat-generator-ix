8ecae1e0c7492896869dbc9412a986fb8230d9a8
Dermot Sheerin <sheerin@avaya.com>
2021-03-12 23:24:54 +0000

    created new modules for logger and routes for Express

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,133 @@
-const express = require("express");
-const router = express.Router();
+// const { chatStatsMap, concurrentCallers,
+//     chatSendMax, firstMsgSendDelay, respondMsgDelay, delayBetweenLoops, agentJoinTimeout,
+//     startTest, startLoop, processAgentJoinEvent, processAgentSendMsgEvent, processAgentDisconnectEvent  } = require("../index.js");
 
-router.get("/", (req, res) => {
-    res.send({ response: "I am alive" }).status(200);
+const index = require("../index.js");
+const { logMessage } = require("../logger/logger");
+const express = require("express");
+const chalk = require("chalk");
+const moment = require("moment");
+const http = require("http");
+const socketIo = require('socket.io');
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json(), cors());
+let interval;
+
+
+// reuse the HTTP server to run socket.io within the same HTTP server instance
+const server = http.createServer(app);
+
+//  call to socketIo() to initialize a new instance by passing in the server object and set origins
+//  option on the server side to allow cross-origin requests
+const io = socketIo(server, {
+    cors: {
+        origin: '*',
+    }});
+
+// using setInterval to emit the chatStats object every second
+// Socket.IO on() takes two arguments: the name of the event, in this case "connection",
+// and a callback which will be executed after every connection event. The connection event returns a socket object which will
+// be passed to the callback function. By using said socket you will be able to send data back to a client in real time.
+io.on("connection", (socket) => {
+    logMessage("New client connected");
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getChatStats(socket), 1000);
+
+    // clearing the interval on any new connection, and on disconnection to avoid flooding the server
+    socket.on("disconnect", () => {
+        logMessage("Client disconnected");
+        clearInterval(interval);
+    });
 });
 
-module.exports = router;
+const getChatStats = socket => {
+    // const response = new Date();
+    // Emitting a new message. Will be consumed by the client
+    socket.emit("FromAPI", index.chatStatsMap);
+};
+
+
+app.post("/allEvents", (req, res) => {
+    // Listen for Agent Join
+    if (req.body.eventType === "PARTICIPANT_ADDED" && req.body.participantType === "AGENT") {
+        res.sendStatus(200);
+        index.processAgentJoinEvent(req.body.engagementId);
+    }
+
+    // Listen for Agent Send Message
+    else if (req.body.senderType === "AGENT") {
+        res.sendStatus(200);
+
+        // after predefined delay respond to Agent message
+        setTimeout(() => {
+            index.processAgentSendMsgEvent(req.body.engagementId);
+        }, index.respondMsgDelay);
+    }
+
+    // Listen for Participant Disconnect
+    else if (req.body.eventType === "PARTICIPANT_DISCONNECTED") {
+        res.sendStatus(200);
+        index.processAgentDisconnectEvent(req.body.engagementId);
+    } else {
+        res.sendStatus(200);
+    }
+});
+
+
+// set Chat Parameters
+app.post("/changeChatParameters", (req, res) => {
+    res.sendStatus(200);
+    index.concurrentCallers = req.body.concurrentCallers;
+    index.chatSendMax = req.body.chatSendMax;
+    index.firstMsgSendDelay = req.body.firstMsgSendDelay;
+    index.respondMsgDelay = req.body.respondMsgDelay;
+    index.delayBetweenLoops = req.body.delayBetweenLoops;
+    index.agentJoinTimeout = req.body.agentJoinTimeout;
+});
+
+// retrieve Chat Parameters
+app.get("/getChatParameters", (req, res) => {
+    res.send({
+        concurrentCallers: index.concurrentCallers,
+        chatSendMax: index.chatSendMax,
+        firstMsgSendDelay: index.firstMsgSendDelay,
+        respondMsgDelay: index.respondMsgDelay,
+        delayBetweenLoops: index.delayBetweenLoops,
+        agentJoinTimeout: index.agentJoinTimeout,
+    });
+});
+
+// GET to retrieve the chatStatsMap details
+app.get("/getStats", (req, res) => {
+    res.send(index.chatStatsMap);
+});
+
+//GET to stop test gradually
+app.get("/stopTest", (req, res) => {
+    index.startLoop = false;
+    res.send(`******** Test will terminate gracefully ********`);
+});
+
+//GET to start test
+app.get("/startTest", (req, res) => {
+    //startLoop = true;
+    index.startTest();
+    logMessage(chalk.green("###### Chat Generator Started ######"));
+    res.send(`******** Test Starting ********`);
+});
+
+//GET to start test
+app.get("/demoTest", (req, res) => {
+    logMessage(chalk.green("###### Demo of pipeline code added ######"));
+    logMessage(`Here is agentJoinTimeout ${index.agentJoinTimeout}`)
+    res.send(`******** Demo Complete 4 ********`);
+});
+
+module.exports.server = server;
+
+
