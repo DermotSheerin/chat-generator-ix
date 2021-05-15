af212e9e93ab74b0f576665b930eda451fafae21
Dermot Sheerin <sheerin@avaya.com>
2021-05-15 09:53:34 +0100

    set docker IP

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let {
 const utils = require("./utilities/utils.js");
 
 const port = 8001;
-const ip = "135.123.64.47";
+const ip = "10.134.45.26";
 
 const promiseMap = {};
 const engagementDetailsMap = {};
